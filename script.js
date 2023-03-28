d650f8e99659eb62b8d8ccca89f403dd76567065
divyakondakrindi <118252095+divyakondakrindi@users.noreply.github.com>
2023-03-28 15:51:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
 //your code here
+const images = document.querySelectorAll('.image');
+
+let dragSrcEl = null;
+
+function handleDragStart(e) {
+  this.classList.add('selected');
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+function handleDragOver(e) {
+  if (e.preventDefault) {
+    e.preventDefault();
+  }
+
+  e.dataTransfer.dropEffect = 'move';
+
+  return false;
+}
+
+function handleDragEnter(e) {
+  this.classList.add('over');
+}
+
+function handleDragLeave(e) {
+  this.classList.remove('over');
+}
+
+function handleDrop(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation();
+  }
+
+  if (dragSrcEl !== this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+
+  return false;
+}
+
+function handleDragEnd(e) {
+  this.classList.remove('selected');
+
+  images.forEach(function (image) {
+    image.classList.remove('over');
+  });
+}
+
+images.forEach(function (image) {
+  image.addEventListener('dragstart', handleDragStart, false);
+  image.addEventListener('dragenter', handleDragEnter, false);
+  image.addEventListener('dragover', handleDragOver, false);
+  image.addEventListener('dragleave', handleDragLeave, false);
+  image.addEventListener('drop', handleDrop, false);
+  image.addEventListener('dragend', handleDragEnd, false);
+});
